8fa51ebd1e0c470faec48d811952c1ca173e5cce
Ian Okumu <okumu.otsembo@gmail.com>
2023-02-07 12:01:31 +0300

    Part 1: Done

--- main.rb
+++ main.rb
@@ -2,36 +2,54 @@
 
 # 1.0 Printing Methods
 # TODO: puts
+puts "Hello Ruby"
 
 # TODO: print
+print "Hola Ruby"
 
-# TODO: pp
+# TODO: pp (pretty printing)
+pp [1,2,3]
+
 
 # 2.0 Strings
 # TODO: CREATE A STRING
+last_name = "Doe"
 
 # TODO: CREATE A STRING WITH SINGLE QUOTES
+first_name = 'Jane'
 
 # TODO: INTERPOLATE STRINGS
+full_name = "First Name is #{first_name}. Last Name is #{last_name}."
 
 # TODO: String methods (upcase, downcase, capitalize)
+puts full_name.upcase
+puts full_name
+
+puts full_name.capitalize
 
 # 3.0 Numbers
 # TODO: Integers
+height = 100
 
 # TODO: Float
+temp = 55.5
 
 # TODO: Number methods (floor, ceil, to_i, to_f)
+puts temp.floor
+puts temp.ceil
 
 
 # 4.0 Boolean
 # TODO: True / Truthy
+isTuesday = true
 
 # TODO: False / Falsy
+isWednesday = false
 
 # 5.0 Symbols
 # TODO: Create symbol
-
+:green
+puts :green
 
 # 6.0 Arrays
 # TODO: Create array natively
